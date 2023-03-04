7a43084c7b8cdc474fcde33067b23b8a78af34f9
Mario Tobar <104882791+mario-th@users.noreply.github.com>
2023-03-04 12:03:39 -0600

    Add files via upload

---
+++ ParOInpar/ParOInpar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Programa para verificar si un numero es par o inpar. *****");
+
+        Console.Write("Ingrese el numero entero que desea verificar: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int verificacion = numero % 2;
+
+        if(verificacion == 0)
+        {
+            Console.WriteLine($"El numero {numero} es Par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numero} es inpar");
+        }
+    }
+}
